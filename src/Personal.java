44418c2fdde3d2874bfba3cdb1d45383bc13ef94
iiparraguirreodaw1 <iiparraguirreodaw1@gmail.com>
2021-04-01 17:30:05 +0200

    Clase Personal terminado.

--- src/Personal.java
+++ src/Personal.java
@@ -1,31 +1,31 @@
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
-public class Personal extends Contacto{
+
+public class Personal extends Contacto {
 
 	private LocalDate fecha;
 	DateTimeFormatter formateador;
 	private Relacion rel;
 	private final String FIRMA = "Un abrazo!!";
-	
-	
+
 	public Personal(String nombre, String apellidos, String telefono, String email, String fecha, Relacion relacion) {
 		super(nombre, apellidos, telefono, email);
 		String[] split = fecha.split("/");
-        this.fecha = LocalDate.of(Integer.parseInt(split[2]), Integer.parseInt(split[1]), Integer.parseInt(split[0]));       
-        formateador = DateTimeFormatter.ofPattern("dd MMM yyyy", new Locale("es", "ES"));
-		 rel = relacion;
+		this.fecha = LocalDate.of(Integer.parseInt(split[2]), Integer.parseInt(split[1]), Integer.parseInt(split[0]));
+		formateador = DateTimeFormatter.ofPattern("dd MMM yyyy", new Locale("es", "ES"));
+		rel = relacion;
 	}
-	
+
 	@Override
-	public String firma() {	
+	public String firma() {
 		return this.FIRMA;
 	}
-	
+
 	public boolean esCumpleaños() {
 		LocalDate ahora = LocalDate.now();
-		if(ahora.getDayOfMonth() == this.fecha.getDayOfMonth()) {
-			if(ahora.getMonth().equals(this.fecha.getMonth())) {
+		if (ahora.getDayOfMonth() == this.fecha.getDayOfMonth()) {
+			if (ahora.getMonth().equals(this.fecha.getMonth())) {
 				return true;
 			}
 		}
@@ -50,7 +50,7 @@ public class Personal extends Contacto{
 
 	@Override
 	public String toString() {
-		return super.toString() +  "Fecha cumpleaños: " + getFecha();
+		return super.toString() + "Fecha cumpleaños: " + getFecha();
 	}
-	
+
 }
