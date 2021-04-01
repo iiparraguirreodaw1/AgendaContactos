610b82ac3b31509b870b2a3e057426df3929e670
jgarciatdaw1 <jgarciatdaw1@gmail.com>
2021-04-01 11:55:01 +0200

    Clase profesional actualizada

--- src/Profesional.java
+++ src/Profesional.java
@@ -1,8 +1,37 @@
+import java.util.Random;
 
-public class Profesional {
+public class Profesional extends Contacto {
 
-	public Profesional() {
+	private String nombreEmpresa;
+	private Random rnd;
+	private final String[] SALUDOS = {"Atentamente", "Saludos", "Saludos cordiales", "Mis mejores deseos"};
 
+	public Profesional(String nombre, String apellidos, String telefono, String email, String nombreEmpresa) {
+		super(nombre, apellidos, telefono, email);
+		this.nombreEmpresa = parsearEmpresa(nombreEmpresa);
+		this.rnd = new Random();
 	}
 
+	private String parsearEmpresa(String nombre) {
+		String output = "";
+		String[] split = nombre.split(" ");
+		for (String nombreSpliteado : split) {
+			output += nombreSpliteado.substring(0, 1).toUpperCase()
+				    + nombreSpliteado.substring(1, nombreSpliteado.length()) + " ";
+		}
+		return output;
+	}
+	
+	public String firma() {
+		return SALUDOS[rnd.nextInt(SALUDOS.length)];
+	}
+	
+	public String getNombreEmpresa() {
+		return nombreEmpresa;
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + "Empresa: " + nombreEmpresa;
+	}
 }
